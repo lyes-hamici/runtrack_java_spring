f6a9748b16111d959dab4d4f3a66a98ca2b613c9
lyes-hamici <lyes.hamici@laplateforme.io>
2025-03-31 10:54:38 +0200

    create : creation of all files

---
+++ jour01/Job05/src/test/java/com/example/jour01/Jour01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jour01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jour01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
